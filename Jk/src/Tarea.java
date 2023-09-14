6e88416ee3938deff487dbbaaaaa730bfa6e2003
estef144z <144652985+estef144z@users.noreply.github.com>
2023-09-14 00:47:07 -0500

    Update Tarea.java

--- Jk/src/Tarea.java
+++ Jk/src/Tarea.java
@@ -3,6 +3,7 @@ public class Tarea {
         final IntIp ip = new IntIp((short) 4, (short) 4, (short) 4, (short) 4);
         ip.Codificar((short) 4, (short) 4, (short) 4, (short) 4);
         System.out.println("La dirección IP es: " + ip.ToString());
+        System.out.println("La dirección IP es: " + ip.ToString());
 
     }
 }
