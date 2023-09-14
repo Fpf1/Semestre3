96210371f915b6e8395f74dd68c699d717deb111
estef144z <144652985+estef144z@users.noreply.github.com>
2023-09-14 00:38:17 -0500

    ya esta
    ya lo compuse por que dani aveces esta medio nahual.

--- Jk/src/Tarea.java
+++ Jk/src/Tarea.java
@@ -1,6 +1,6 @@
-public class App {
+public class Tarea {
     public static void main(String[] args) throws Exception {
-        IntIp ip = new IntIp((short) 4, (short) 4, (short) 4, (short) 4);
+        final IntIp ip = new IntIp((short) 4, (short) 4, (short) 4, (short) 4);
         ip.Codificar((short) 4, (short) 4, (short) 4, (short) 4);
         System.out.println("La dirección IP es: " + ip.ToString());
 
