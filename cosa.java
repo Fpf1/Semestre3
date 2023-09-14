e53d884160070bc1238566bed3c0e1265bb4dcfc
Fpf1 <115376402+Fpf1@users.noreply.github.com>
2023-09-13 22:59:46 -0500

    Update cosa.java
    Ya le cambie más jijiji

--- cosa.java
+++ cosa.java
@@ -6,5 +6,9 @@ public class cosa {
     public static void main(String[] args) {
         System.out.println("Hola");
         System.out.println("Hola");
+        System.out.println("Hola");
+        System.out.println("Hola");
+        System.out.println("Hola");
+
     }
 }
