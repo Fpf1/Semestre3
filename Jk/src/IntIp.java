f82ae8b35ae3742b01b923b920394f2f734b317c
Fpf1 <115376402+Fpf1@users.noreply.github.com>
2023-09-15 09:48:51 -0500

    Yessss

--- Jk/src/IntIp.java
+++ Jk/src/IntIp.java
@@ -22,27 +22,28 @@ public class IntIp {
         this.myIp = this.myIp << 8;
         this.myIp = this.myIp | oct4;
     }
-//Al fin sirve
+
+    // Al fin sirve
     public IntIp(String cadenaIp) {
         int posicionPunto = cadenaIp.indexOf('.');
         String cadOct1 = cadenaIp.substring(0, posicionPunto);
-        
+
         int posicionSegPunto = cadenaIp.indexOf('.', posicionPunto + 1);
         String cadOct2 = cadenaIp.substring(posicionPunto + 1, posicionSegPunto);
-        
+
         int posicionTercerPunto = cadenaIp.indexOf('.', posicionSegPunto + 1);
         String cadOct3 = cadenaIp.substring(posicionSegPunto + 1, posicionTercerPunto);
-        
+
         String cadOct4 = cadenaIp.substring(posicionTercerPunto + 1);
-        
+
         short numOct1 = Short.parseShort(cadOct1);
         short numOct2 = Short.parseShort(cadOct2);
         short numOct3 = Short.parseShort(cadOct3);
         short numOct4 = Short.parseShort(cadOct4);
-        
+
         this.codificar(numOct1, numOct2, numOct3, numOct4);
-    }    
-    
+    }
+
     public void codificar(short oct1, short oct2, short oct3, short oct4) {
         // Asignar a myIp los octetos a nivel de bits
         this.myIp = this.myIp | oct1;
@@ -54,12 +55,11 @@ public class IntIp {
         this.myIp = this.myIp | oct4;
     }
 
-  
     /**
-     * @name    toString - Devuelve la ip como una cadena de bits
-     * @return    the sum of x and y
+     * @name toString - Devuelve la ip como una cadena de bits
+     * @return the sum of x and y
      */
     public String toString() {
-     return Integer.toBinaryString(this.myIp);
+        return Integer.toBinaryString(this.myIp);
     }
 }
--- Jk/src/Tarea.java
+++ Jk/src/Tarea.java
@@ -4,16 +4,16 @@
 public class Tarea {
 
     public static void main(String[] args) {
-              //Creación del objeto 
+        // Creación del objeto
         IntIp ipPc = new IntIp((short) 192, (short) 168, (short) 25, (short) 3);
-        
-        //imprimir contenido de ip
+
+        // imprimir contenido de ip
         System.out.println("La dirección IP es: " + ipPc);
-        
+
         IntIp otraIp = new IntIp("192.168.25.3");
-        
-        //imprimir contenido de ip
+
+        // imprimir contenido de ip
         System.out.println("La dirección IP es: " + otraIp);
-        
+
     }
 }
