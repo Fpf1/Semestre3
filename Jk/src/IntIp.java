0059a3ac23139f40e81d142d56fb6957c192b981
Fpf1 <115376402+Fpf1@users.noreply.github.com>
2023-09-14 22:41:30 -0500

    Lo que subio el profe en teams
    shhh

--- Jk/src/IntIp.java
+++ Jk/src/IntIp.java
@@ -1,17 +1,19 @@
-public class IntIp {
-    private int myIp;
-//jijii
-    public void IntIp(short oct1, short oct2, short oct3, short oct4) {
-        this.myIp = this.myIp | oct1;
-        this.myIp = this.myIp << 8;
-        this.myIp = this.myIp | oct2;
-        this.myIp = this.myIp << 8;
-        this.myIp = this.myIp | oct3;
-        this.myIp = this.myIp << 8;
-        this.myIp = this.myIp | oct4;
-    }
 
-    public void Codificar(short oct1, short oct2, short oct3, short oct4) {
+/**
+ * Write a description of class IntIp here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class IntIp {
+    // instance variables - replace the example below with your own
+    private int myIp;
+
+    /**
+     * Constructor for objects of class IntIp
+     */
+    public IntIp(short oct1, short oct2, short oct3, short oct4) {
+        // Asignar a myIp los octetos a nivel de bits
         this.myIp = this.myIp | oct1;
         this.myIp = this.myIp << 8;
         this.myIp = this.myIp | oct2;
@@ -23,31 +25,41 @@ public class IntIp {
 
     public IntIp(String cadenaIp) {
         int posicionPunto = cadenaIp.indexOf('.');
-        String cadOct1 = cadenaIp.substring(posicionPunto -1);
-
-        int posicionSegPunto = cadOct1.indexOf('.', posicionPunto + 1);
-        String cadOct2 = cadenaIp.substring(posicionPunto + 1, posicionSegPunto - 1);
-
+        String cadOct1 = cadenaIp.substring(0, posicionPunto);
+        
+        int posicionSegPunto = cadenaIp.indexOf('.', posicionPunto + 1);
+        String cadOct2 = cadenaIp.substring(posicionPunto + 1, posicionSegPunto);
+        
         int posicionTercerPunto = cadenaIp.indexOf('.', posicionSegPunto + 1);
-        String cadOct3 = cadenaIp.substring(posicionSegPunto + 1, posicionTercerPunto - 1);
-
+        String cadOct3 = cadenaIp.substring(posicionSegPunto + 1, posicionTercerPunto);
+        
         String cadOct4 = cadenaIp.substring(posicionTercerPunto + 1);
-
+        
         short numOct1 = Short.parseShort(cadOct1);
         
         short numOct3 = Short.parseShort(cadOct3);
         short numOct4 = Short.parseShort(cadOct4);
-
-
-        this.Codificar(numOct1, numOct2, numOct3, numOct4);
+        
+        this.codificar(numOct1, numOct2, numOct3, numOct4);
+    }    
+    
+    public void codificar(short oct1, short oct2, short oct3, short oct4) {
+        // Asignar a myIp los octetos a nivel de bits
+        this.myIp = this.myIp | oct1;
+        this.myIp = this.myIp << 8;
+        this.myIp = this.myIp | oct2;
+        this.myIp = this.myIp << 8;
+        this.myIp = this.myIp | oct3;
+        this.myIp = this.myIp << 8;
+        this.myIp = this.myIp | oct4;
     }
 
-    public IntIp(short s, short t, short u, short v) {
-    }
-
-    public String ToString() {
-        return Integer.toBinaryString(myIp);
-
+  
+    /**
+     * @name    toString - Devuelve la ip como una cadena de bits
+     * @return    the sum of x and y
+     */
+    public String toString() {
+     return Integer.toBinaryString(this.myIp);
     }
 }
- 
