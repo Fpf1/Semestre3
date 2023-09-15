177388c0c7945b0bc0ce0de1ab9a93b45a39a4d9
Fpf1 <115376402+Fpf1@users.noreply.github.com>
2023-09-14 22:52:31 -0500

    Update Tarea.java
    Al fin sirve

---
+++ Jk/src/Tarea.java
@@ -0,0 +1,19 @@
+/**
+ * Tarea
+ */
+public class Tarea {
+
+    public static void main(String[] args) {
+              //Creación del objeto 
+        IntIp ipPc = new IntIp((short) 192, (short) 168, (short) 25, (short) 3);
+        
+        //imprimir contenido de ip
+        System.out.println("La dirección IP es: " + ipPc);
+        
+        IntIp otraIp = new IntIp("192.168.25.3");
+        
+        //imprimir contenido de ip
+        System.out.println("La dirección IP es: " + otraIp);
+        
+    }
+}
